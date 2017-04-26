25e6bfdceb591582d712c9bb5fbc0ff6767e28f7
Frzurita <zurit92@gmail.com>
2017-04-27 00:40:51 +0200

    removing unnecessary files

--- src/api/localConfig.js
+++
@@ -1,8 +0,0 @@
-const _host = 'http://localhost:8000'
-const _photosHost = 'http://res.cloudinary.com/glovoapp/f_auto,q_auto/'
-// const _picturesCDN = 'http://res.cloudinary.com/glovoapp/image/fetch/http://devsite.glovoapp.com/'
-const _picturesCDN = 'http://localhost:8000/'
-
-export const host = _host
-export const photosHost = _photosHost
-export const picturesCDN = _picturesCDN
